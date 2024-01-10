d21f46aa8d104f36243eadd9c53751d1dd687fbd
mojnuapp <thisismojnu@gmail.com>
2024-01-10 18:23:12 +0600

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>React Bangla Tutorial</span>,
+  project: {
+    link: 'https://github.com/codebymojnu/react-bangla-tutorial',
+  },
+  chat: {
+    link: 'https://discord.com/mojnu13',
+  },
+  docsRepositoryBase: 'https://github.com/codebymojnu/react-bangla-tutorial',
+  footer: {
+    text: 'React Bangla Tutorial',
+  },
+}
+
+export default config

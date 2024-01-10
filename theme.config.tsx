6c08526f79edcb8138ef156aba824fa61ece4e19
mojnuapp <thisismojnu@gmail.com>
2024-01-10 22:14:11 +0600

    title change

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from "next/router";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  title: "React Bangla Tutorial",
   logo: <span>React Bangla Tutorial</span>,
   project: {
-    link: 'https://github.com/codebymojnu/react-bangla-tutorial',
+    link: "https://github.com/codebymojnu/react-bangla-tutorial",
   },
   chat: {
-    link: 'https://discord.com/mojnu13',
+    link: "https://discord.com/mojnu13",
   },
-  docsRepositoryBase: 'https://github.com/codebymojnu/react-bangla-tutorial',
+  docsRepositoryBase: "https://github.com/codebymojnu/react-bangla-tutorial",
   footer: {
-    text: 'React Bangla Tutorial',
+    text: "React Bangla Tutorial",
   },
-}
+  useNextSeoProps() {
+    const { route } = useRouter();
+    if (route !== "/") {
+      return {
+        titleTemplate: "%s – React Bangla Tutorial",
+      };
+    }
+    return { titleTemplate: "React Bangla Tutorial" };
+  },
+};
 
-export default config
+export default config;
